23cbde938b67d3923dce4181aeb4c48abbc63c76
eka-martin <99816833+eka-martin@users.noreply.github.com>
2022-07-28 13:04:43 +0200

    1

--- front/js/script.js
+++ front/js/script.js
@@ -1,14 +1,20 @@
 // https://developer.mozilla.org/en-US/docs/Web/API/Response/json
+console.log('test');
 
 fetch('http://localhost:3000/api/products')
     .then((response) => {
         return response.json();
     })
     .then((data) => {
-        for (const product of data.products) {
-            const listItem = document.createElement('a');
-            let elt = document.getElementById('items');
-            elt.appendChild(listItem);
+        console.log('test2', data);
+        let elt = document.getElementById('items');
+        for (const product of data) {
+            const lien = document.createElement('a');
+            lien.href = 'product.html?id=' + product._id;
+            const art = document.createElement('article');
+            art.innerHTML = product.name;
+            lien.appendChild(art);
+            elt.appendChild(lien);
         }
     })
     .catch(console.error);
