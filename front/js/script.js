ca0b7272dd9c15fd6ee87a4adf6ddda97a02038b
eka-martin <99816833+eka-martin@users.noreply.github.com>
2022-07-29 20:24:26 +0200

    items

--- front/js/script.js
+++ front/js/script.js
@@ -1,5 +1,5 @@
 // https://developer.mozilla.org/en-US/docs/Web/API/Response/json
-console.log('test');
+
 
 fetch('http://localhost:3000/api/products')
     .then((response) => {
@@ -8,13 +8,25 @@ fetch('http://localhost:3000/api/products')
     .then((data) => {
         console.log('test2', data);
         let elt = document.getElementById('items');
+        elt.classList.add('items');
         for (const product of data) {
             const lien = document.createElement('a');
             lien.href = 'product.html?id=' + product._id;
             const art = document.createElement('article');
-            art.innerHTML = product.name;
+            const name = document.createElement('h3');
+            name.innerHTML = product.name;
+            const des = document.createElement('p');
+            des.innerHTML = product.description;
+            const image = document.createElement('img');
+            image.src = product.imageUrl;
+            image.alt = product.altTxt;
             lien.appendChild(art);
+            art.appendChild(name);
             elt.appendChild(lien);
+            art.appendChild(des);
+            art.appendChild(image);
+            des.classList.add('productDescription');
+
         }
     })
     .catch(console.error);
