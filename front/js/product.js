dbc925693153d494f5a99cb3bbe18c391b3751d4
eka-martin <99816833+eka-martin@users.noreply.github.com>
2022-08-02 12:30:22 +0200

    1

--- front/js/product.js
+++ front/js/product.js
@@ -1,11 +1,48 @@
-let url_str = 'window.location.href';
-let url = new URL(url_str);
+
+let url = new URL(window.location.href);
 let search_params = url.searchParams;
 
 
-if (search_params.has('id')) {
+const id = url.searchParams.get('id');
+console.log(id);
 
-    console.log(search_params.get('id'))
-}
+// if (search_params.has('id')) {
 
-console.log('test');
+//     console.log(search_params.get('id'))
+// }
+
+
+// let url = new URL(window.location.href);
+// let search_params = url.searchParams;
+// console.log(search_params.get('id'));
+
+// let response = fetch('http://localhost:3000/api/service/${id}');
+// let response = fetch('http://localhost:3000/api/products/{product-ID}');
+
+// let response = await fetch(`http://localhost:3000/api/products/${id}`)
+//    .then((response) => {
+//        return response.json();
+//    })
+
+
+//https://stackoverflow.com/questions/54268951/react-fetchapi-how-to-take-data-from-related-json-by-id
+
+fetch(`http://localhost:3000/api/products/${id}`)
+    .then((response) => {
+        return response.json();
+    })
+    .then((data) => {
+        console.log(data);
+    })
+
+
+
+
+
+
+
+
+
+
+
+
