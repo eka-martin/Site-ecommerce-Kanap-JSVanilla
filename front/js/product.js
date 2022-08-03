3071898393558e7e457216baed5c58c77845a7ad
eka-martin <99816833+eka-martin@users.noreply.github.com>
2022-08-03 17:54:28 +0200

    produit

--- front/js/product.js
+++ front/js/product.js
@@ -6,7 +6,56 @@ let search_params = url.searchParams;
 const id = url.searchParams.get('id');
 console.log(id);
 
-// if (search_params.has('id')) {
+
+fetch(`http://localhost:3000/api/products/${id}`)
+    .then((response) => {
+        return response.json();
+    })
+    .then((data) => {
+        console.log(data);
+
+        const name = document.getElementById('title');
+        name.innerHTML = data.name;
+        const des = document.getElementById('description');
+        des.innerHTML = data.description;
+        const prix = document.getElementById('price');
+        prix.innerHTML = data.price;
+        const div = document.querySelector('.item__img')
+        let image = document.createElement('img');
+        image.src = "../images/logo.png";
+        image.alt = "Photographie d'un canapé";
+        div.appendChild(image);
+        const clr = document.getElementById('colors');
+        // const colors = data.colors;
+        // console.log(colors);
+        for (let color in data.colors) {
+            let opt = document.createElement('option');
+            clr.appendChild(opt);
+            opt.value = `${color}`;
+
+        }
+
+
+    })
+    .catch(console.error);
+
+const btn_envoyerPanier = document.getElementById('addToCart');
+btn_envoyerPanier.addEventListener('click', (e) => {
+    e.preventDefault();
+})
+
+// let optionProduit = {
+//             id_produit:
+//     color:
+//            quantite:
+
+// }
+
+
+
+
+
+    // if (search_params.has('id')) {
 
 //     console.log(search_params.get('id'))
 // }
@@ -26,36 +75,6 @@ console.log(id);
 
 
 //https://stackoverflow.com/questions/54268951/react-fetchapi-how-to-take-data-from-related-json-by-id
-
-fetch(`http://localhost:3000/api/products/${id}`)
-    .then((response) => {
-        return response.json();
-    })
-    .then((data) => {
-        console.log(data);
-        // let elt = document.querySelector('.item');
-        // const art = document.createElement('article');
-
-
-        const name = document.getElementById('title');
-        name.innerHTML = data.name;
-        const des = document.getElementById('description');
-        des.innerHTML = data.description;
-        const prix = document.getElementById('price');
-        prix.innerHTML = data.price;
-
-        // elt.appendChild(art);
-        // art.appendChild(name);
-        // art.appendChild(des);
-        // art.appendChild(prix);
-
-
-
-    })
-    .catch(console.error);
-
-
-
 
 
 
